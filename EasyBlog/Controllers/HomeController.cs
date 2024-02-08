638556a9afb8667d8d0b769db0c3805775849986
mehme <mehmetyigit.mail@gmail.com>
2024-02-08 11:52:07 +0300

    Show post , filter category

--- EasyBlog/Controllers/HomeController.cs
+++ EasyBlog/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
+using EasyBlog.Data;
 using EasyBlog.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace EasyBlog.Controllers
@@ -7,20 +9,39 @@ namespace EasyBlog.Controllers
 	public class HomeController : Controller
 	{
 		private readonly ILogger<HomeController> _logger;
+		private readonly ApplicationDbContext _db;
 
-		public HomeController(ILogger<HomeController> logger)
+		public HomeController(ILogger<HomeController> logger, ApplicationDbContext db)
 		{
 			_logger = logger;
+			_db = db;
 		}
 
-		public IActionResult Index()
+		public IActionResult Index(int? cid)
 		{
-			return View();
+			IQueryable<Post> posts = cid == null ? _db.Posts : _db.Posts.Where(x => x.CategoryId == cid);
+
+			ViewBag.CategoryName = _db.Categories.Find(cid)?.Name;
+
+			return View(posts.OrderByDescending(x => x.Id).ToList());
 		}
 
 		public IActionResult Privacy()
 		{
 			return View();
+		}
+
+
+		[Route("Post/{id:int}")]
+		public IActionResult ShowPost(int id)
+		{
+			Post post = _db.Posts.Include(x => x.Category).FirstOrDefault(x => x.Id == id);
+
+			if (post == null)
+			{
+				return NotFound();
+			}
+			return View(post);
 		}
 
 		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
